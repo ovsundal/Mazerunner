b7049d48806849e9e6e38cfa0074c51e160fe069
ovsundal <osundal83@gmail.com>
2017-10-06 22:37:10 +0200

    feat: Add InformationObject class

---
+++ src/mazeoblig/InformationObject.java
@@ -0,0 +1,85 @@
+package mazeoblig;
+
+import simulator.ClientCallbackInterface;
+import simulator.PositionInMaze;
+import java.awt.*;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+
+public class InformationObject implements Serializable {
+
+    //info provided by client
+    private Integer clientId;
+    private PositionInMaze position;
+    private Color color;
+    private ClientCallbackInterface clientCallbackInterface;
+
+    //info provided by server
+    private int timeSinceServerStarted;
+    private int totalServerMessagesReceived;
+    private int totalClientMessagesSent;
+    HashMap<Integer, InformationObject> listOfAllClientPositions;
+
+    public InformationObject(ClientCallbackInterface cb, Color color) throws RemoteException {
+        super();
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public PositionInMaze getPosition() {
+        return position;
+    }
+
+    public void setPosition(PositionInMaze position) {
+        this.position = position;
+    }
+
+    public int getTimeSinceServerStarted() {
+        return timeSinceServerStarted;
+    }
+
+    public void setTimeSinceServerStarted(int timeSinceServerStarted) {
+        this.timeSinceServerStarted = timeSinceServerStarted;
+    }
+
+    public int getTotalServerMessagesReceived() {
+        return totalServerMessagesReceived;
+    }
+
+    public void setTotalServerMessagesReceived(int totalServerMessagesReceived) {
+        this.totalServerMessagesReceived = totalServerMessagesReceived;
+    }
+
+    public int getTotalClientMessagesSent() {
+        return totalClientMessagesSent;
+    }
+
+    public void setTotalClientMessagesSent(int totalClientMessagesSent) {
+        this.totalClientMessagesSent = totalClientMessagesSent;
+    }
+
+    public Integer getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Integer clientId) {
+        this.clientId = clientId;
+    }
+
+    public ClientCallbackInterface getClientCallbackInterface() {
+        return clientCallbackInterface;
+    }
+
+    public void setClientCallbackInterface(ClientCallbackInterface clientCallbackInterface) {
+        this.clientCallbackInterface = clientCallbackInterface;
+    }
+}
